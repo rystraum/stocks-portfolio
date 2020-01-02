2495c295524228db3e3ebea7cc9cc176d9bfba28
Rystraum Gamonez <rystraum@gmail.com>
2020-01-02 13:04:38 +0800

    Auto-backup on create

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,13 @@
+# frozen_string_literal: true
+
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
+  after_commit :backup
+
+  def backup
+    dest = "#{Dir.home}/Dropbox/Finances/Stocks/DB"
+    return unless File.directory?(dest)
+
+    `cp #{Rails.root.join('db')}/development.sqlite3 #{dest}/backup-#{DateTime.now.to_formatted_s(:iso8601).parameterize}.sqlite3`
+  end
 end
