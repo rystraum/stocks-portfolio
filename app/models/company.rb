3cb526d1d6c32369454f03eea692c08b912950de
Rystraum Gamonez <rystraum@gmail.com>
2021-02-16 11:05:53 +0800

    WIP fetching of company information from PSE

--- app/models/company.rb
+++ app/models/company.rb
@@ -64,7 +64,11 @@ class Company < ApplicationRecord
 
   def history
     @history ||= (activities + stock_dividends + cash_dividends).sort_by do |thing|
-      thing.is_a?(Activity) ? thing.date : (thing.ex_date.blank? ? thing.pay_date : thing.ex_date)
+      if thing.is_a?(Activity)
+        thing.date
+      else
+        (thing.ex_date.blank? ? thing.pay_date : thing.ex_date)
+      end
     end
   end
 
@@ -80,30 +84,38 @@ class Company < ApplicationRecord
     require 'net/http'
     require 'uri'
 
-    uri = URI.parse('https://www.pse.com.ph/stockMarket/companyInfoHistoricalData.html?method=getRecentSecurityQuoteData&ajax=true')
+    uri = URI.parse('https://edge.pse.com.ph/common/DisclosureCht.ax')
     request = Net::HTTP::Post.new(uri)
-    request.content_type = 'application/x-www-form-urlencoded; charset=UTF-8'
+    data = {
+      'cmpy_id': pse_company_id.to_s,
+      'security_id': pse_security_id.to_s,
+      'startDate': 30.days.ago.strftime('%m-%d-%Y'),
+      'endDate': Date.today.strftime('%m-%d-%Y')
+    }.to_json.unpack('C*')
+
     request['Connection'] = 'keep-alive'
     request['Pragma'] = 'no-cache'
     request['Cache-Control'] = 'no-cache'
     request['Dnt'] = '1'
     request['X-Requested-With'] = 'XMLHttpRequest'
-    request['User-Agent'] = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.129 Safari/537.36'
-    request['Accept'] = '*/*'
-    request['Origin'] = 'https://www.pse.com.ph'
+    request['User-Agent'] = 'User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36'
+    request['Accept'] = 'Accept: application/json, text/javascript, */*; q=0.01'
+    request['Origin'] = 'https://edge.pse.com.ph'
+    request['Content-Type'] = 'application/json'
     request['Sec-Fetch-Site'] = 'same-origin'
     request['Sec-Fetch-Mode'] = 'cors'
     request['Sec-Fetch-Dest'] = 'empty'
-    request['Referer'] = "https://www.pse.com.ph/stockMarket/companyInfo.html?id=#{pse_company_id}&security=#{pse_security_id}&tab=0"
+    request['Referer'] = "https://edge.pse.com.ph/companyPage/stockData.do?cmpy_id=#{pse_company_id}&security=#{pse_security_id}"
     request['Accept-Language'] = 'en-US,en;q=0.9'
-    request['Cookie'] = 'cookieconsent_status=dismiss; JSESSIONID=f1c6408c99945028c5731c7670793530dd1fbeabe2b4094556e57e886ae0b10a.e38NbNeRbx0Pa40Lc3mMa3qQah4Me0'
-    request.set_form_data(
-      'security' => pse_security_id.to_s
-    )
+    request['Cookie'] = 'Cookie: JSESSIONID=uhHyB6YqPkdIEjpEWZ9hVwrH.server-ep; BIGipServerPOOL_EDGE=1427584378.20480.0000; access=approve'
+    request.content_type = 'application/octet-stream'
+    request.set_form_data(data)
 
     req_options = {
       use_ssl: uri.scheme == 'https'
     }
+
+    binding.pry
 
     response = Net::HTTP.start(uri.hostname, uri.port, req_options) do |http|
       http.request(request)
