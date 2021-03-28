c894e7bc52c5b128ae4e77efa4bed7c4fb8e9613
Rystraum Gamonez <rystraum@gmail.com>
2021-03-28 15:46:02 +0800

    Added ApplicationRecord.to_csv

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -4,6 +4,24 @@ class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
   after_commit :backup
 
+  def self.to_csv
+    require 'csv'
+
+    base = "#{Dir.home}/Dropbox/Finances/Stocks/CSV/#{DateTime.now.to_date.to_s}"
+    FileUtils.mkdir_p base
+
+    csv_string = CSV.generate do |csv|
+      csv << column_names
+      all.each do |record|
+        csv << column_names.collect do |column|
+          record[column]
+        end
+      end
+    end
+
+    File.write("#{base}/#{table_name}.csv", csv_string)
+  end
+
   def backup
     dest = "#{Dir.home}/Dropbox/Finances/Stocks/DB"
     return unless File.directory?(dest)
