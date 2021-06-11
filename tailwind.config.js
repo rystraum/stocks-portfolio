8e857079acb6b476b422fa46b0211c6c1abb3390
Rystraum Gamonez <rystraum@gmail.com>
2021-06-11 16:47:23 +0800

    JIT mode and other Tailwind Plugins

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,17 @@
 module.exports = {
-  purge: [],
-  darkMode: false, // or 'media' or 'class'
-  theme: {
-    extend: {},
-  },
-  variants: {
-    extend: {},
-  },
-  plugins: [],
-}
+    mode: "jit",
+    purge: ["./public/**/*.html"],
+    darkMode: false, // or 'media' or 'class'
+    theme: {
+        extend: {},
+    },
+    variants: {
+        extend: {},
+    },
+    plugins: [
+        require("@tailwindcss/typography"),
+        require("@tailwindcss/forms"),
+        require("@tailwindcss/line-clamp"),
+        require("@tailwindcss/aspect-ratio")
+    ],
+};
