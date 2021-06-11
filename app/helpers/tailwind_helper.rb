2200c3e9ebecd74c3ac79249f313ea3866b49c96
Rystraum Gamonez <rystraum@gmail.com>
2021-06-12 01:15:34 +0800

    Converted topmenu to tailwind

---
+++ app/helpers/tailwind_helper.rb
@@ -0,0 +1,50 @@
+module TailwindHelper
+  def header_link_to(text, path, **args)
+    is_active_path = request.path == path
+    
+    if is_active_path
+      css_class = "bg-gray-900 text-white px-3 py-2 rounded-md text-sm font-medium"
+      args[:aria] ||= {}
+      args[:aria][:current] = "page"
+    else
+      css_class = "text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+    end
+
+    args[:class] = "#{args[:class]} #{css_class}"
+    link_to(text, path, args)
+  end
+
+  def home_icon
+    i = <<~HEREDOC
+      <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6" />
+      </svg>
+    HEREDOC
+    i.html_safe
+  end
+
+  def chevron_down_icon
+    i = <<~HEREDOC
+      <svg class="w-5 h-5 ml-2 -mr-1" viewBox="0 0 20 20" fill="currentColor"><path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
+    HEREDOC
+    i.html_safe
+  end
+
+  def speaker_icon
+    i = <<~HEREDOC
+      <svg class="h-6 w-6 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
+        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M11 5.882V19.24a1.76 1.76 0 01-3.417.592l-2.147-6.15M18 13a3 3 0 100-6M5.436 13.683A4.001 4.001 0 017 6h1.832c4.1 0 7.625-1.234 9.168-3v14c-1.543-1.766-5.067-3-9.168-3H7a3.988 3.988 0 01-1.564-.317z" />
+      </svg>
+    HEREDOC
+    i.html_safe
+  end
+
+  def x_icon
+    i = <<~HEREDOC
+      <svg class="h-6 w-6 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
+        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
+      </svg>
+    HEREDOC
+    i.html_safe
+  end
+end
