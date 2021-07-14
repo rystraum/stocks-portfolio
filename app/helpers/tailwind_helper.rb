d82099344ea40c07947253b2fd893312676e0eef
Rystraum Gamonez <rystraum@gmail.com>
2021-07-14 16:42:10 +0800

    Added chevron_left_icon

--- app/helpers/tailwind_helper.rb
+++ app/helpers/tailwind_helper.rb
@@ -32,6 +32,15 @@ module TailwindHelper
     i.html_safe
   end
 
+  def chevron_left_icon
+    i = <<~HEREDOC
+      <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 19l-7-7 7-7" />
+      </svg>
+    HEREDOC
+    i.html_safe
+  end
+
   def speaker_icon
     i = <<~HEREDOC
       <svg class="h-6 w-6 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
