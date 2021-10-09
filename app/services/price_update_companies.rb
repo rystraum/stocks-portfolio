8d5467ebf32040d57e2f4f777b8258ad271ffc37
Rystraum Gamonez <rystraum@gmail.com>
2021-10-09 11:22:35 +0800

    Shuffle company order before updating so if ever we're blocked by PSE, later companies still get updated

--- app/services/price_update_companies.rb
+++ app/services/price_update_companies.rb
@@ -16,7 +16,7 @@ class PriceUpdateCompanies
   end
 
   def run_from_console
-    @companies.each.with_index do |company, index|
+    @companies.to_a.shuffle.each.with_index do |company, index|
       next if !company.can_update_from_pse?      
       puts "== #{company.ticker} Starting price update"
       begin
