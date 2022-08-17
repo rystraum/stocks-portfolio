303b6b3495876812dc3afaefd000ea3f6a89c20f
Rystraum Gamonez <rystraum@gmail.com>
2022-08-17 22:03:34 +0800

    Added configuration for Rails 6.1.6.1 Security Patch

--- config/application.rb
+++ config/application.rb
@@ -14,6 +14,8 @@ module StocksPortfolio
     config.load_defaults 5.2
     config.time_zone = 'Singapore'
 
+    config.active_record.yaml_column_permitted_classes = [ActiveSupport::HashWithIndifferentAccess, BigDecimal]
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
