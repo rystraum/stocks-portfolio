2740a513004c162c101d803a74b74ce65224f995
Rystraum Gamonez <rystraum@gmail.com>
2023-03-06 23:16:31 +0800

    Added Marketwatch link

--- app/components/company_info_component.rb
+++ app/components/company_info_component.rb
@@ -4,7 +4,16 @@ class CompanyInfoComponent < ViewComponent::Base
     @company = company
   end
 
+  def with_company_name?
+    !@company.name.blank?
+  end
+
   def simply_wall_st_url
+    return "" if !with_company_name?
     "https://simplywall.st/stocks/ph/media/pse-#{@company.ticker}/#{@company.company_name.parameterize}-shares"
+  end
+
+  def marketwatch_url
+    "https://www.marketwatch.com/investing/stock/#{@company.ticker}?countrycode=ph"
   end
 end
