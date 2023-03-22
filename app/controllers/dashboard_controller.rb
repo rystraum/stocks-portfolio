4fd4e8626c561d09100c228a67f2e82165ebccab
Rystraum Gamonez <rystraum@gmail.com>
2023-03-22 18:19:04 +0800

    Hide pages based on permission

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -8,18 +8,19 @@ class DashboardController < ApplicationController
 
   def update_prices
     portfolio_companies = UserPortfolio.new(current_user).companies
-    update_results = portfolio_companies.collect do |company|
-      PSE.new(company).price_update!&.persisted?
+    update_results = portfolio_companies.collect.with_index do |company, index|
+      next if !company.can_update_from_pse?
+      PriceUpdateJob.set(wait: 2.seconds * index).perform_later(company)
     rescue RuntimeError
       false
     end
 
-    message = if update_results.all?
-      "All portfolio company prices updated"
-    else
-      "Some portfolio company prices failed to update"
-    end
+    # message = if update_results.all?
+    #   "All portfolio company prices updated"
+    # else
+    #   "Some portfolio company prices failed to update"
+    # end
 
-    redirect_back(fallback_location: root_path, alert: message)
+    redirect_back(fallback_location: root_path, alert: "Update is being done in the background. Check back after a couple of minutes.")
   end
 end
