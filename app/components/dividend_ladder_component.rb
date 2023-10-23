f5b38d0e3dd48726077650e5a77b6da5d86b03d0
Rystraum Gamonez <rystraum@gmail.com>
2023-10-23 22:36:27 +0800

    Include monthly average dividends

--- app/components/dividend_ladder_component.rb
+++ app/components/dividend_ladder_component.rb
@@ -3,11 +3,16 @@ class DividendLadderComponent < ViewComponent::Base
     @costs_calculator = costs_calculator
     @set = set
     @year = year.to_s
+    @dividends_this_year = year == 2023 ? (@set.sums[@year] + 4281) : @set.sums[@year]
     @costs_this_year = costs_calculator.costs_as_of(year: year.to_i, month: 12)
   end
 
+  def average_monthly_dividends
+    helpers.format_currency(@dividends_this_year / 12)
+  end
+
   def total_this_year
-    helpers.format_currency @set.sums[@year]
+    helpers.format_currency @dividends_this_year
   end
 
   def formatted_costs_this_year
@@ -15,6 +20,6 @@ class DividendLadderComponent < ViewComponent::Base
   end
 
   def percent_dividends_this_year
-    helpers.format_percentage(@set.sums[@year], @costs_this_year)
+    helpers.format_percentage(@dividends_this_year, @costs_this_year)
   end
 end
