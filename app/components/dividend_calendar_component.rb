12c5d8b802f718ce4572026d712498ea1636ea02
Rystraum Gamonez <rystraum@gmail.com>
2025-02-04 03:08:45 +0800

    Added Net YoY calculations in Dividend Ladder & Calendar components

--- app/components/dividend_calendar_component.rb
+++ app/components/dividend_calendar_component.rb
@@ -6,6 +6,7 @@ class DividendCalendarComponent < ViewComponent::Base
     @year = year.to_s
     @dividends_this_year = @set.sums[@year] || 0
     @costs_this_year = costs_calculator.costs_as_of(year: year.to_i, month: 12)
+    @costs_last_year = costs_calculator.costs_as_of(year: year.to_i - 1, month: 12)
   end
 
   def average_monthly_dividends
@@ -22,5 +23,17 @@ class DividendCalendarComponent < ViewComponent::Base
 
   def percent_dividends_this_year
     helpers.format_percentage(@dividends_this_year, @costs_this_year)
+  end
+
+  def net_yoy_costs_change
+    helpers.format_currency(@costs_this_year - @costs_last_year)
+  end
+
+  def percent_dividends_yoy_costs_change
+    helpers.format_percentage(@dividends_this_year, @costs_this_year - @costs_last_year)
+  end
+
+  def yoy_costs_change_less_dividends
+    helpers.format_currency(@costs_this_year - @costs_last_year - @dividends_this_year)
   end
 end
--- app/components/dividend_ladder_component.rb
+++ app/components/dividend_ladder_component.rb
@@ -5,6 +5,7 @@ class DividendLadderComponent < ViewComponent::Base
     @year = year.to_s
     @dividends_this_year = @set.sums[@year] || 0
     @costs_this_year = costs_calculator.costs_as_of(year: year.to_i, month: 12)
+    @costs_last_year = costs_calculator.costs_as_of(year: year.to_i - 1, month: 12)
   end
 
   def average_monthly_dividends
@@ -21,5 +22,17 @@ class DividendLadderComponent < ViewComponent::Base
 
   def percent_dividends_this_year
     helpers.format_percentage(@dividends_this_year, @costs_this_year)
+  end
+
+  def net_yoy_costs_change
+    helpers.format_currency(@costs_this_year - @costs_last_year)
+  end
+
+  def percent_dividends_yoy_costs_change
+    helpers.format_percentage(@dividends_this_year, @costs_this_year - @costs_last_year)
+  end
+
+  def yoy_costs_change_less_dividends
+    helpers.format_currency(@costs_this_year - @costs_last_year - @dividends_this_year)
   end
 end
