c7a6398885806707215677d0f903c6babef3e910
Rystraum Gamonez <rystraum@gmail.com>
2025-02-04 02:28:37 +0800

    Add Company#target_buy_price

---
+++ db/migrate/20250203182135_add_target_buy_price_to_companies.rb
@@ -0,0 +1,5 @@
+class AddTargetBuyPriceToCompanies < ActiveRecord::Migration[6.1]
+  def change
+    add_column :companies, :target_buy_price, :decimal, precision: 15, scale: 2
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2024_12_17_081627) do
+ActiveRecord::Schema.define(version: 2025_02_03_182135) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -55,6 +55,7 @@ ActiveRecord::Schema.define(version: 2024_12_17_081627) do
     t.string "pse_security_id"
     t.string "pse_company_id"
     t.string "name"
+    t.decimal "target_buy_price", precision: 15, scale: 2
     t.index ["industry"], name: "index_companies_on_industry"
     t.index ["ticker"], name: "index_companies_on_ticker", unique: true
   end
