427ed008329e7e7d3d16ac2a835582567eb1b9be
Rystraum Gamonez <rystraum@gmail.com>
2025-02-06 00:52:06 +0800

    Alpine plugin init

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,9 +3,11 @@ import 'styles/application.scss';
 import Rails from '@rails/ujs';
 import * as ActiveStorage from '@rails/activestorage';
 import Alpine from 'alpinejs';
+import focus from '@alpinejs/focus'
 
 window.Alpine = Alpine;
 
 Rails.start();
 ActiveStorage.start();
+Alpine.plugin(focus);
 Alpine.start();
