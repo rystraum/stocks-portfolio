97d120acd67b19edc124d74b108af4ad323d1d90
Rystraum Gamonez <rystraum@gmail.com>
2025-03-26 00:36:39 +0800

    Filter by portfolio companies

--- app/controllers/dividend_announcements_controller.rb
+++ app/controllers/dividend_announcements_controller.rb
@@ -3,12 +3,21 @@
 class DividendAnnouncementsController < ApplicationController
   def index
     params[:future_only] ||= '1'
+    params[:company_scope] ||= 'portfolio'
 
     @dividend_announcements = DividendAnnouncement.includes(:company)
                                                   .order(ex_date: :asc)
                                                   .paginate(page: params[:page], per_page: 20)
 
-    @dividend_announcements.where('ex_date >= ?', Date.today) if params[:future_only].to_i == 1
+    if params[:future_only].to_i == 1
+      @dividend_announcements = @dividend_announcements.where(
+        'ex_date >= ?', Date.today
+      )
+    end
+
+    if params[:company_scope] == 'portfolio'
+      @dividend_announcements = @dividend_announcements.where(company_id: current_user.company_ids)
+    end
 
     return unless params[:ticker].present?
 
