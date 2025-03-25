fd5ae6cfff1c41bcce1d5326e75c869a1590cada
Rystraum Gamonez <rystraum@gmail.com>
2025-03-26 00:29:37 +0800

    Allow filtering future announcements

--- app/controllers/dividend_announcements_controller.rb
+++ app/controllers/dividend_announcements_controller.rb
@@ -2,12 +2,16 @@
 
 class DividendAnnouncementsController < ApplicationController
   def index
-    @dividend_announcements = DividendAnnouncement.includes(:company)
-      .order(ex_date: :asc)
-      .paginate(page: params[:page], per_page: 20)
+    params[:future_only] ||= '1'
 
-    if params[:ticker].present?
-      @dividend_announcements = @dividend_announcements.where(companies: { ticker: params[:ticker] })
-    end
+    @dividend_announcements = DividendAnnouncement.includes(:company)
+                                                  .order(ex_date: :asc)
+                                                  .paginate(page: params[:page], per_page: 20)
+
+    @dividend_announcements.where('ex_date >= ?', Date.today) if params[:future_only].to_i == 1
+
+    return unless params[:ticker].present?
+
+    @dividend_announcements = @dividend_announcements.where(companies: { ticker: params[:ticker] })
   end
 end
