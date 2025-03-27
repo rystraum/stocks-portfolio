16391f89798b6247b440e96022049def2bd9a675
Rystraum Gamonez <rystraum@gmail.com>
2025-03-27 12:17:14 +0800

    Show target price notes on hover

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,10 +4,13 @@ import Rails from '@rails/ujs';
 import * as ActiveStorage from '@rails/activestorage';
 import Alpine from 'alpinejs';
 import focus from '@alpinejs/focus'
-
-window.Alpine = Alpine;
+import Tooltip from "@ryangjchandler/alpine-tooltip";
 
 Rails.start();
 ActiveStorage.start();
+
+Alpine.plugin(Tooltip); // https://github.com/ryangjchandler/alpine-tooltip
 Alpine.plugin(focus);
 Alpine.start();
+
+window.Alpine = Alpine;
