d8b37d969b75eb2414aa465e4487e3a2d82ad795
Rystraum Gamonez <rystraum@gmail.com>
2025-04-22 21:39:37 +0800

    Compute PNL of selected activities

--- app/javascript/controllers/crypto_activity_selection_controller.js
+++ app/javascript/controllers/crypto_activity_selection_controller.js
@@ -1,7 +1,7 @@
 import { Controller } from "stimulus";
 
 export default class extends Controller {
-  static targets = ["tableRow", "checkbox", "cryptoSum", "fiatSum", "forexAverage"];
+  static targets = ["tableRow", "checkbox", "cryptoSum", "fiatSum", "forexAverage", "pnl"];
 
   connect() {
     this.updateSums();
@@ -16,6 +16,7 @@ export default class extends Controller {
   updateSums() {
     let cryptoSum = 0;
     let fiatSum = 0;
+    let lastPrice = 0;
     this.checkboxTargets.forEach((cb) => {
       if (cb.checked) {
         // activity_type: 0 = buy, 1 = sell
@@ -24,10 +25,14 @@ export default class extends Controller {
         const fiat = parseFloat(cb.dataset.fiatAmount) - parseFloat(cb.dataset.feeFiat);
         cryptoSum += sign * crypto;
         fiatSum += sign * fiat;
+        lastPrice = parseFloat(cb.dataset.lastPrice);
       }
     });
     this.cryptoSumTarget.textContent = cryptoSum.toLocaleString(undefined, { minimumFractionDigits: 2, maximumFractionDigits: 10 });
     this.fiatSumTarget.textContent = fiatSum.toLocaleString(undefined, { minimumFractionDigits: 2, maximumFractionDigits: 2 });
     this.forexAverageTarget.textContent = cryptoSum !== 0 ? (fiatSum / cryptoSum).toLocaleString(undefined, { minimumFractionDigits: 2, maximumFractionDigits: 2, useGrouping: true }) : 0;
+
+    // hardcoded 0.3% fee
+    this.pnlTarget.textContent = ((cryptoSum * lastPrice * 0.997) - fiatSum).toLocaleString(undefined, { minimumFractionDigits: 2, maximumFractionDigits: 2, useGrouping: true });
   }
 }
