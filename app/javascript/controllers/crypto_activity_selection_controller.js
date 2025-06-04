34e53103b2574f2f32ac58b496edb505ea0281ff
Rystraum Gamonez <rystraum@gmail.com>
2025-06-04 14:18:26 +0800

    Added computation of PNL based on a test price

--- app/javascript/controllers/crypto_activity_selection_controller.js
+++ app/javascript/controllers/crypto_activity_selection_controller.js
@@ -1,7 +1,7 @@
 import { Controller } from "stimulus";
 
 export default class extends Controller {
-  static targets = ["tableRow", "checkbox", "cryptoSum", "fiatSum", "forexAverage", "pnl"];
+  static targets = ["tableRow", "checkbox", "cryptoSum", "fiatSum", "forexAverage", "pnl", "testPrice", "testPnl"];
 
   connect() {
     this.updateSums();
@@ -13,28 +13,86 @@ export default class extends Controller {
     this.updateSums();
   }
 
-  updateSums() {
-    let cryptoSum = 0;
-    let fiatSum = 0;
-    let lastPrice = 0;
-    this.checkboxTargets.forEach((cb) => {
-      if (cb.checked) {
-        // activity_type: 0 = buy, 1 = sell
-        const sign = cb.dataset.activityType === "1" ? -1 : 1;
-        const crypto = parseFloat(cb.dataset.cryptoAmount) - parseFloat(cb.dataset.feeCrypto);
-        const fiat = parseFloat(cb.dataset.fiatAmount) - parseFloat(cb.dataset.feeFiat);
-        cryptoSum += sign * crypto;
-        fiatSum += sign * fiat;
-        lastPrice = parseFloat(cb.dataset.lastPrice);
-      }
-    });
-    this.cryptoSumTarget.textContent = cryptoSum.toLocaleString(undefined, { minimumFractionDigits: 2, maximumFractionDigits: 10 });
-    this.fiatSumTarget.textContent = fiatSum.toLocaleString(undefined, { minimumFractionDigits: 2, maximumFractionDigits: 2 });
-    this.forexAverageTarget.textContent = cryptoSum !== 0 ? (fiatSum / cryptoSum).toLocaleString(undefined, { minimumFractionDigits: 2, maximumFractionDigits: 2, useGrouping: true }) : 0;
+  inputChange(event) {
+    const testPrice = parseFloat(event.target.value.replace(/,/g, ''));
+    this.setTargetPricePnl(testPrice);
+  }
 
+  toggleAll(event) {
+    const checked = event.target.checked;
+    this.tableRowTargets.forEach(row => {
+      const checkbox = row.querySelector("[data-crypto-activity-selection-target='checkbox']");
+      if (checkbox) checkbox.checked = checked;
+    });
+    this.updateSums();
+  }
+
+  updateSums() {
+    const { cryptoSum, fiatSum, lastPrice, buyCryptoSum, buyFiatSum } = this.getSums();
+    const { pnlAmount, pnlPercentage } = this.computePnlWithFee(cryptoSum, lastPrice, fiatSum);
+
+    const testPrice = parseFloat(this.testPriceTarget.value.replace(/,/g, ''));
+    this.setTargetPricePnl(testPrice);
+
+    this.cryptoSumTarget.textContent = this.formatLocale(cryptoSum, 10);
+    this.fiatSumTarget.textContent = this.formatLocale(fiatSum);
+    this.forexAverageTarget.textContent = buyCryptoSum !== 0 ? this.formatLocale(buyFiatSum / buyCryptoSum) : 0;
+    this.pnlTarget.textContent = `${this.formatLocale(pnlAmount)} (${this.formatLocale(pnlPercentage)}%)`;
+  }
+
+  setTargetPricePnl(testPrice) {
+    const { cryptoSum, fiatSum } = this.getSums();
+    const { pnlAmount, pnlPercentage } = this.computePnlWithFee(cryptoSum, testPrice, fiatSum);
+    this.testPnlTarget.textContent = `${this.formatLocale(pnlAmount)} (${this.formatLocale(pnlPercentage)}%)`;
+  }
+
+  formatLocale(number, maximumFractionDigits = 2, useGrouping = true) {
+    return Number(number).toLocaleString(undefined, { minimumFractionDigits: 2, maximumFractionDigits, useGrouping });
+  }
+
+  computePnlWithFee(cryptoSum, lastPrice, fiatSum) {
     // hardcoded 0.3% fee
     const pnlAmount = (cryptoSum * lastPrice * 0.997) - fiatSum;
     const pnlPercentage = pnlAmount / fiatSum * 100;
-    this.pnlTarget.textContent = `${pnlAmount.toLocaleString(undefined, { minimumFractionDigits: 2, maximumFractionDigits: 2, useGrouping: true })} (${pnlPercentage.toLocaleString(undefined, { minimumFractionDigits: 2, maximumFractionDigits: 2, useGrouping: true })}%)`;
+
+    return {
+      pnlAmount,
+      pnlPercentage
+    }
+  }
+
+  getSums() {
+    let lastPrice = 0;
+
+    let buyCryptoSum = 0;
+    let sellCryptoSum = 0;
+    let buyFiatSum = 0;
+    let sellFiatSum = 0;
+    
+    this.checkboxTargets.forEach((cb) => {
+      if (cb.checked) {
+        const isSell = cb.dataset.activityType === "sell";
+        const cryptoAmount = parseFloat(cb.dataset.cryptoAmount);
+        const cryptoFee = parseFloat(cb.dataset.feeCrypto);
+        const fiatAmount = parseFloat(cb.dataset.fiatAmount);
+        const fiatFee = parseFloat(cb.dataset.feeFiat);
+        const crypto = isSell ? cryptoAmount : cryptoAmount - cryptoFee;
+        const fiat = isSell ? fiatAmount - fiatFee : fiatAmount;
+        isSell ? sellCryptoSum += crypto : buyCryptoSum += crypto;
+        isSell ? sellFiatSum += fiat : buyFiatSum += fiat;
+
+        lastPrice = parseFloat(cb.dataset.lastPrice);
+      }
+    });
+
+    return {
+      cryptoSum: buyCryptoSum - sellCryptoSum,
+      fiatSum: buyFiatSum - sellFiatSum,
+      buyCryptoSum,
+      sellCryptoSum,
+      buyFiatSum,
+      sellFiatSum,
+      lastPrice
+    }
   }
 }
