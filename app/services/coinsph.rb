b795b0284229ef9dceacdbbae2caf1a638ca2797
Rystraum Gamonez <rystraum@gmail.com>
2025-08-25 01:23:51 +0800

    Added Coinsph.account

--- app/services/coinsph.rb
+++ app/services/coinsph.rb
@@ -1,10 +1,19 @@
+# frozen_string_literal: true
+
 class Coinsph
+  def self.apikey
+    Rails.application.credentials.coinsph_apikey
+  end
+
+  def self.secret_key
+    Rails.application.credentials.coinsph_secretkey
+  end
+
   def self.update_all!
     tickers = CryptoCurrency.pluck(:ticker).collect { |ticker| "#{ticker.upcase}PHP" }
-    apikey = Rails.application.credentials.coinsph_apikey
     response = HTTParty.get(
-      "https://api.pro.coins.ph/openapi/quote/v1/ticker/price?symbols=[#{tickers.join(",")}]", 
-      headers: { "X-COINS-APIKEY" => apikey }
+      "https://api.pro.coins.ph/openapi/quote/v1/ticker/price?symbols=[#{tickers.join(',')}]",
+      headers: { 'X-COINS-APIKEY' => apikey },
     )
     # Response looks like this:
     #   [
@@ -21,11 +30,28 @@ class Coinsph
     #         "price": "189"
     #     }
     # ]
-    
+
     response.parsed_response.each do |coin|
-      crypto_currency = CryptoCurrency.find_by(ticker: coin["symbol"].gsub("PHP", ""))
+      crypto_currency = CryptoCurrency.find_by(ticker: coin['symbol'].gsub('PHP', ''))
       next if crypto_currency.nil?
-      crypto_currency.update(last_price: coin["price"].to_d, last_price_at: Time.now)
+
+      crypto_currency.update(last_price: coin['price'].to_d, last_price_at: Time.now)
     end
+  end
+
+  def self.account
+    get_with_signature('https://api.pro.coins.ph/openapi/v1/account', { timestamp: Time.now.to_i * 1_000 })
+  end
+
+  def self.get_with_signature(url, params)
+    query_string = URI.encode_www_form(params)
+    signature = OpenSSL::HMAC.hexdigest('SHA256', secret_key, query_string)
+
+    options = {
+      query: params.merge(signature: signature),
+      headers: { 'X-COINS-APIKEY' => apikey }
+    }
+
+    HTTParty.get(url, options)
   end
 end
---
+++ test/services/coinsph_test.rb
@@ -0,0 +1,40 @@
+# frozen_string_literal: true
+
+require 'test_helper'
+
+# rubocop:disable Metrics/MethodLength
+class CoinsphMock
+  def self.account
+    {
+      'canTrade' => true,
+      'canDeposit' => true,
+      'canWithdraw' => true,
+      'accountType' => 'SPOT',
+      'updateTime' => 1_701_790_415_739,
+      'balances' =>
+  [{ 'asset' => 'AIR', 'free' => '0.53673664', 'locked' => '0' },
+   { 'asset' => 'BTC', 'free' => '0', 'locked' => '0' },
+   { 'asset' => 'ETH', 'free' => '0.00000098', 'locked' => '0' },
+   { 'asset' => 'HIPPO', 'free' => '938.04548872', 'locked' => '0' },
+   { 'asset' => 'IOST', 'free' => '501.58762862', 'locked' => '0' },
+   { 'asset' => 'LAT', 'free' => '4.74383301', 'locked' => '0' },
+   { 'asset' => 'MATIC', 'free' => '0', 'locked' => '0' },
+   { 'asset' => 'NPC', 'free' => '207.79756853', 'locked' => '0' },
+   { 'asset' => 'PHP', 'free' => '0.23', 'locked' => '0' },
+   { 'asset' => 'PHPC', 'free' => '0', 'locked' => '0' },
+   { 'asset' => 'POL', 'free' => '0', 'locked' => '0' },
+   { 'asset' => 'SUI', 'free' => '0', 'locked' => '0' },
+   { 'asset' => 'SUNDOG', 'free' => '56.78464308', 'locked' => '0' },
+   { 'asset' => 'USDT', 'free' => '108.00901091', 'locked' => '0' },
+   { 'asset' => 'XRP', 'free' => '0.002223', 'locked' => '0' }],
+      'token' => 'PHP',
+      'daily' => { 'cashInLimit' => '1000000', 'cashInRemaining' => '1000000', 'cashOutLimit' => '1000000',
+                   'cashOutRemaining' => '1000000', 'totalWithdrawLimit' => '1000000', 'totalWithdrawRemaining' => '1000000' },
+      'monthly' => { 'cashInLimit' => '30000000', 'cashInRemaining' => '29992500', 'cashOutLimit' => '30000000',
+                     'cashOutRemaining' => '30000000', 'totalWithdrawLimit' => '30000000', 'totalWithdrawRemaining' => '29810144.77' },
+      'annually' => { 'cashInLimit' => '360000000', 'cashInRemaining' => '359925509.54', 'cashOutLimit' => '360000000',
+                      'cashOutRemaining' => '360000000', 'totalWithdrawLimit' => '360000000', 'totalWithdrawRemaining' => '359696437.16' }
+    }
+  end
+end
+# rubocop:enable Metrics/MethodLength
