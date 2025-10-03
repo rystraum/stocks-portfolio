7fa66d7772c6f659e877e35dc682a416b34d26e5
Rystraum Gamonez <rystraum@gmail.com>
2025-10-03 13:30:48 +0800

    Convert datasource to dropdown

--- app/models/crypto_currency.rb
+++ app/models/crypto_currency.rb
@@ -14,13 +14,32 @@
 #
 
 class CryptoCurrency < ApplicationRecord
+  COINSPH_DATASOURCE = "https://api.pro.coins.ph/".freeze
+  COINMARKETCAP_DATASOURCE = "https://pro-api.coinmarketcap.com/".freeze
+
   self.primary_key = :id
   validates :name, presence: true
   validates :ticker, presence: true, uniqueness: true
+  validate :datasource_ticker_validation
 
   scope :alphabetical, -> { order(:ticker) }
-  scope :coinsph, -> { where(datasource: "https://api.pro.coins.ph/") }
-  scope :coinmarketcap, -> { where(datasource: "https://pro-api.coinmarketcap.com/") }
+  scope :coinsph, -> { where(datasource: COINSPH_DATASOURCE) }
+  scope :coinmarketcap, -> { where(datasource: COINMARKETCAP_DATASOURCE) }
+
+  def coinmarketcap?
+    datasource == COINMARKETCAP_DATASOURCE
+  end
+
+  def coinsph?
+    datasource == COINSPH_DATASOURCE
+  end
+
+  def datasource_ticker_validation
+    return true if datasource.nil?
+    
+    errors.add(:datasource_ticker, "is required & must be an ID") if coinmarketcap? && (datasource_ticker.blank? || Float(datasource_ticker, exception: false).nil?)
+    errors.add(:datasource_ticker, "is required") if coinsph? && datasource_ticker.blank?
+  end
 
   def fiat
     return 'USDT' if ticker.end_with?('USDT') && ticker != 'USDT'
