4f9b45ac996bc6accceb8706102d9ba94dc14018
Rystraum Gamonez <rystraum@gmail.com>
2025-10-14 19:36:52 +0800

    Fill in values for stock portfolio

--- app/controllers/portfolio_controller.rb
+++ app/controllers/portfolio_controller.rb
@@ -2,9 +2,12 @@ require 'ostruct'
 
 class PortfolioController < AuthenticatedUserController
   def home
-    @cash_data = PortfolioItem.new(spent: 1000.00, current_value: 1200.00)
-    @uitf_data = PortfolioItem.new(spent: 500.00, current_value: 500.00)
-    @stock_data = PortfolioItem.new(spent: 2500.00, current_value: 3000.00)
-    @crypto_data = PortfolioItem.new(spent: 1000.00, current_value: 800.00)
+    companies = UserPortfolio.new(current_user).companies
+    company_set = CompanySet.new(companies, current_user)
+
+    @cash_data = PortfolioItem.new(spent: 0, current_value: 0)
+    @uitf_data = PortfolioItem.new(spent: 0, current_value: 0)
+    @stock_data = PortfolioItem.new(spent: company_set.total_costs, current_value: company_set.total_value)
+    @crypto_data = PortfolioItem.new(spent: 0, current_value: 0)
   end
 end
